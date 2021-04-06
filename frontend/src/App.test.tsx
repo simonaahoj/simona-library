4f306d4e77bb266eaa9ac1a9242ccd71bbc48389
simona <simonaahoj@gmail.com>
2021-04-06 21:04:24 +0200

    secrets add to action

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
+import { Provider } from 'react-redux'
+import makeStore from './redux/store'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const store = makeStore()
+test('renders all ISBN library', async () => {
+  const {} = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+
+  const ISBNLirary = await screen.getByText('ISBN')
+  expect(ISBNLirary).toBeInTheDocument()
+})
